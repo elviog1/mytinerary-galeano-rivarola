852f696e6b10dec30afff3c085f2897c52cc19e7
juanrivarola <jeryflr@gmail.com>
2022-08-18 16:49:53 -0300

    home page

---
+++ src/components/WelcomeComponent.js
@@ -0,0 +1,13 @@
+import '../styles/Welcome.css'
+
+function WelcomeComponent(){
+    return(
+        <div className='App-container'>
+        <h1 className='App-title'>MyTinerary</h1>
+        <p className='App-title-description'>The best slogan is No Slogan</p>
+        <button className='App-title-button'>Let´s Go!</button>
+        </div>
+         )
+}
+
+export default WelcomeComponent
