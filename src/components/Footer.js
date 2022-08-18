06fc94d1097be58b1c59784a056fccd09718b53d
juanrivarola <jeryflr@gmail.com>
2022-08-18 18:08:00 -0300

    creacion del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../styles/Footer.css'
 
 export default function Footer() {
   return (
     <div className='Footer-div'>
-        <p>Mytinerary</p>
+      <p className='Footer-title'><span className='Footer-my'>My</span>Tinerary</p>
+      <div>
+        <p className='Footer-direccion'>Rivadavia 3472</p>
+        </div>
+        <div className='Footer-redes'>
+          <img className='Footer-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/479px-WhatsApp.svg.png" alt="logo-whatsapp" />
+          <img className='Footer-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="logo-whatsapp" />
+          <img className='Footer-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png" alt="logo-whatsapp" />         
+        </div>
+
     </div>
   )
 }
