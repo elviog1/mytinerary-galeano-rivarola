4c9e089b432191cb731df9bddb2671c5af5a81d2
juanrivarola <jeryflr@gmail.com>
2022-08-20 16:44:40 -0300

    añadi estilos

--- src/components/CountryCarousel.js
+++ src/components/CountryCarousel.js
@@ -1,25 +1,27 @@
 import Carousel from "./Carousel";
 
+
 function CountryCarousel (){
     const items =[
-        {url: "./logo.192", country: "Athens"},
-        {url: "/logo.192", country: "Buenos Aires"},
-        {url: "/logo.192", country: "Havana"},
-        {url: "/logo.192", country: "Las Vegas"},
-        {url: "", country: "Lima"},
-        {url: "", country: "London"},
-        {url: "", country: "Los Angeles"},
-        {url: "", country: "Paris"},
-        {url: "", country: "Rio de Janeiro"},
-        {url: "", country: "Rome"},
-        {url: "", country: "Sydney"},
-        {url: "", country: "Tokyo"},
+        // {url: "/img/Athens.jpg", country: "Athens"},
+        // {url: "/img/BuenosAires.jpg", country: "Buenos Aires"},
+        // {url: "/img/Havana.jpg", country: "Havana"},
+        // {url: "/img/LasVegas.jpg", country: "Las Vegas"},
+        // {url: "/img/Lima.jpg", country: "Lima"},
+        // {url: "/img/London.jpg", country: "London"},
+        // {url: "/img/LosAngeles.jpg", country: "Los Angeles"},
+        // {url: "/img/Paris.jpg", country: "Paris"},
+        // {url: "/img/RioDeJaneiro.jpg", country: "Rio de Janeiro"},
+         {url: "/img/Rome.jpg", country: "Rome"},
+        // {url: "/img/Sydney.jpg", country: "Sydney"},
+        // {url: "/img/Tokyo.jpg", country: "Tokyo"},
 ]
     return (
         <>
-            <Carousel data={items} rangeStart={0} rangeEnd={4}/>
-            {/* <Carousel data={items} rangeStart={4} rangeEnd={8}/>
-            <Carousel data={items} rangeStart={8} rangeEnd={12}/> */}
+            <h1 className='Carousel-title'>Popular MYtineraries</h1>
+            <Carousel data={items} rangeStart={0} rangeEnd={3}/>
+            {/* <Carousel data={items} rangeStart={4} rangeEnd={7}/>
+            <Carousel data={items} rangeStart={8} rangeEnd={11}/> no se por que no aparecen*/}
         </>
     )
 }
