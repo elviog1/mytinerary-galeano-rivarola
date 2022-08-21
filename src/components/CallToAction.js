c3f194be7c9f9286017af48ce782fe31045999d1
juanrivarola <jeryflr@gmail.com>
2022-08-21 18:22:09 -0300

    faltaba el return

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -2,10 +2,13 @@ import '../styles/Welcome.css'
 import React from 'react'
 
 function CallToAction() {
-    <>
-    <img src='/img/ArrowUp.gif' alt='Arrow Up' className='Arrow-up'/>
-    <p className='App-button-description'>Click here to see the best destinys</p>
-    </>
+    return (
+        <>
+        <img src='/img/ArrowUp.gif' alt='Arrow Up' className='Arrow-up'/>
+        <p className='App-button-description'>Click here to see the best destinys</p>
+        </>
+    )
+
 }
 
 export default CallToAction
