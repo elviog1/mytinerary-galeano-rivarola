4578681ab368e12495332d5db54e3711f10c9779
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-23 20:39:21 -0300

    creacion de todas las cities

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,8 +1,28 @@
+import CardCity from "../components/CardCity"
+
 
 export default function Cities(){
+    const items =[
+        {url: "/img/Athens.jpg", country: "Athens"},
+        {url: "/img/BuenosAires.jpg", country: "Buenos Aires"},
+        {url: "/img/Havana.jpg", country: "Havana"},
+        {url: "/img/LasVegas.jpg", country: "Las Vegas"},
+        {url: "/img/Lima.jpg", country: "Lima"},
+        {url: "/img/London.jpg", country: "London"},
+        {url: "/img/LosAngeles.jpg", country: "Los Angeles"},
+        {url: "/img/Paris.jpg", country: "Paris"},
+        {url: "/img/RioDeJaneiro.jpg", country: "Rio de Janeiro"},
+        {url: "/img/Rome.jpg", country: "Rome"},
+        {url: "/img/Sydney.jpg", country: "Sydney"},
+        {url: "/img/Tokyo.jpg", country: "Tokyo"},
+]
+
+
     return(
-        <>
-            <h1>Cities</h1>
-        </>
+
+            <div>
+               <CardCity data={items} />
+            </div>
+
     )
 }
