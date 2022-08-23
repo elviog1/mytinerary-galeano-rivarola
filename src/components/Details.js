fae27aeaf595318afdd62091a8b5f23d69987971
juanrivarola <jeryflr@gmail.com>
2022-08-23 13:19:08 -0300

    futura pag de details

---
+++ src/components/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function Details() {
+const{id} = useParams()
+
+  return (
+    <div>
+        <h1>{id}</h1>
+    </div>
+  )
+}
