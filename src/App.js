6b4fa1f2b60ed91cf5bb47bd0631a3c067015d26
juanrivarola <jeryflr@gmail.com>
2022-08-23 13:22:45 -0300

    añadi rutas

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import WelcomeComponent from './pages/WelcomeComponent';
 import LayoutBasico from './layouts/LayoutBasico';
 import CountryCarousel from './components/CountryCarousel'
 import UnderConstruccion from './pages/UnderConstruction';
-
-
-
-
+import Cities from './pages/Cities'
+import NewCity from './pages/NewCity';
+import Details from './components/Details';
+import ScrollToTop from './components/ScrollToTop';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return(
       <>
+      <BrowserRouter>
+      <ScrollToTop />
       <LayoutBasico>
-      <WelcomeComponent />
-      <CountryCarousel />
-      {/* <UnderConstruccion /> */}
+      <Routes>
+      <Route path='/' element={<WelcomeComponent/> }/>
+      <Route path='/cities' element={<Cities/>}/>  
+      <Route path='/*' element={<UnderConstruccion />}/>
+      <Route path='/newcity' element={<NewCity />}/>
+      <Route path='/cities/:id' element={<Details />}/>
+      </Routes>
       </LayoutBasico>
-      </>
+      </BrowserRouter>
+  </>
   ) 
-
 }
 
 
