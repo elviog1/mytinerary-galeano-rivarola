bf4caa0422a98006d1f33980daf93eaa4f3e27b1
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-23 16:34:38 -0300

    logo y links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import {Link as LinkRouter} from 'react-router-dom'
-
+import '../styles/Header.css'
 const pages = [
     {name: 'Home', to:'/'},
     {name: 'Cities', to: '/cities'},
-    {name: 'New City', to:'/newcity'},
+    {name: 'NewCity', to:'/newcity'},
 ]
 
-const link = (page) => <LinkRouter to={page.to}>{page.name}</LinkRouter>
+const link = (page) => <LinkRouter to={page.to} className={page.name}>{page.name}</LinkRouter>
 
 function Header() {
     return (
-        <div>
-             {pages.map(link)}
+        <div className='Header'>
+            <div className='Header-nav'>
+                <h1 className='Header-logo'>DreamJuan</h1>
+                <div className='Header-links'>
+                    {pages.map(link)}
+                </div>
+            </div>
+        <img src='https://cdn-icons-png.flaticon.com/512/3293/3293331.png' className='Header-avatar'></img>
         </div>
     )
 }
