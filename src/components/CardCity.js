d41c5543073021530f0ac0116946c535e6306864
juanrivarola <jeryflr@gmail.com>
2022-08-24 14:15:55 -0300

    añadi pais

--- src/components/CardCity.js
+++ src/components/CardCity.js
@@ -7,6 +7,7 @@ function CardCity(props){
         <div className="CardCity-container">
             <div className='CardCity-info'>
                 <img src={item.url} className="CardCity-img" />
+                <p className="CardCity-city">{item.city}</p>
                 <p className="CardCity-country">{item.country}</p>
             </div>
         </div>
