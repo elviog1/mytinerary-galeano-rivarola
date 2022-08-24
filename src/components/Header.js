51408a636b586e98f62f6c2e23d695be1946864c
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-24 03:38:21 -0300

    se agrego la hamburguesa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {Link as LinkRouter} from 'react-router-dom'
 import '../styles/Header.css'
 const pages = [
@@ -9,15 +10,26 @@ const pages = [
 const link = (page) => <LinkRouter to={page.to} className={page.name}>{page.name}</LinkRouter>
 
 function Header() {
+    
+    const [isActive,setActive] = useState("false")
+    const handleToggle = () =>{
+        setActive(!isActive)
+    }
+    
+
     return (
         <div className='Header'>
-            <div className='Header-nav'>
+            <div className='Header-nav '>
+                <div className={isActive ? 'active': null} id="active"> 
+                    {pages.map(link)}
+                </div>
+                <button onClick={handleToggle}><img className='Header-burger ' src='https://img.icons8.com/doodle/344/menu.png'/></button>
                 <h1 className='Header-logo'><span>Dream</span>Juan</h1>
                 <div className='Header-links'>
                     {pages.map(link)}
                 </div>
             </div>
-        <img src='https://cdn-icons-png.flaticon.com/512/3293/3293331.png' className='Header-avatar'></img>
+        <img src='https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-login-web-store-flaticons-lineal-color-flat-icons-3.png' className='Header-avatar'></img>
         </div>
     )
 }
