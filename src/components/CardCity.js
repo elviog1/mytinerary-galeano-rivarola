4695a9c0bb7d9c615479522958c295a55200e4b3
juanrivarola <jeryflr@gmail.com>
2022-08-24 17:28:14 -0300

    linkie a not found

--- src/components/CardCity.js
+++ src/components/CardCity.js
@@ -1,16 +1,21 @@
 import '../styles/Cities.css'
+import {Link as LinkRouter} from 'react-router-dom'
+
 function CardCity(props){
     const allCard = props.data
 
 
     const card =(item) =>(
-        <div className="CardCity-container">
+            <LinkRouter to='notfound'>
+            <div className="CardCity-container">
             <div className='CardCity-info'>
                 <img src={item.url} className="CardCity-img" />
                 <p className="CardCity-city">{item.city}</p>
                 <p className="CardCity-country">{item.country}</p>
             </div>
         </div>
+            </LinkRouter>
+
     )
 
     return(
