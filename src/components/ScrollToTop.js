370666d908dca408e1ab4356b62c7cdd396a2635
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-26 01:12:37 -0300

    se borro un import innecesario

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import {useLocation} from 'react-router-dom'
 
 export default function ScrollToTop(){
-
     const {pathname} = useLocation()
     
     useEffect(() => {
