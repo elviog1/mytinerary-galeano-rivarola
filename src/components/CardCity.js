f7a53e38cdcacf655bfe9c921673da5fdfe0f07f
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-31 01:59:42 -0300

    se agrego mas informacion a las card

--- src/components/CardCity.js
+++ src/components/CardCity.js
@@ -10,9 +10,13 @@ function CardCity(props){
             <LinkRouter to='notfound'>
             <div className="CardCity-container">
             <div className='CardCity-info'>
-                <img src={item.url} className="CardCity-img" />
-                <p className="CardCity-city">{item.city}</p>
+                <img src={item.image} className="CardCity-img" />
+                <p className="CardCity-city">{item.name}</p>
                 <p className="CardCity-country">{item.country}</p>
+                <div className='cardCity-info'>
+                    <p className='cardCity-population'>Population: {item.population.toLocaleString("de-DE")}</p>
+                    <p className='cardCity-fundation'>Fundation: {item.fundation.toLocaleString("de-DE")}</p>
+                </div>
             </div>
         </div>
             </LinkRouter>
