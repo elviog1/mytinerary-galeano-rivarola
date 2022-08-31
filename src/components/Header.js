7845b60068c0043da5b5aab56bf40482198c462a
juanrivarola <jeryflr@gmail.com>
2022-08-31 15:39:29 -0300

    añadi edit city al nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const pages = [
     {name: 'Home', to:'/'},
     {name: 'Cities', to: '/cities'},
     {name: 'New City', to:'/newcity'},
+    {name: 'Edit city', to:'/editcity'}
 ]
 
 const link = (page) => <LinkRouter to={page.to} className='nav-item'>{page.name}</LinkRouter>
@@ -23,13 +24,13 @@ function Header() {
                 <div className={isActive ? 'active': null} id="active"> 
                     {pages.map(link)}
                 </div>
-                <button className='boton-burger' onClick={handleToggle}><img className='Header-burger ' src='https://img.icons8.com/doodle/344/menu.png'/></button>
+                <button className='boton-burger' onClick={handleToggle}><img className='Header-burger ' src='https://img.icons8.com/doodle/344/menu.png' href='burger menu'/></button>
                 <h1 className='Header-logo'><span>My</span>Tinerary</h1>
                 <div className='Header-links'>
                     {pages.map(link)}
                 </div>
             </div>
-        <img src='https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-login-web-store-flaticons-lineal-color-flat-icons-3.png' className='Header-avatar'></img>
+        <img src='https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-login-web-store-flaticons-lineal-color-flat-icons-3.png' className='Header-avatar' href='profile picture'></img>
         </div>
     )
 }
