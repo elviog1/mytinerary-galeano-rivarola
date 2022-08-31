d934fde59d1ee2bf19b29ad4b712c213e2f53601
juanrivarola <jeryflr@gmail.com>
2022-08-31 12:59:27 -0300

    intento de hacer que se renderize de la DB

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
 
 export default function Details() {
-const{id} = useParams()
+  const id = useParams()
+  const[items, setItems] = useState({})
+  useEffect(()=>{
+  axios.get("http://localhost:4000/cities/" + id)
+      .then(response => console.log(response)/*setItems(response.data),*/
+      // console.log(items),
+      )
+    }, [])
+  
+    console.log(id);
+    // console.log(item);
 
   return (
     <div>
-        <h1>{id}</h1>
+        <h1>hoalassssssssssdl</h1>
     </div>
   )
 }
