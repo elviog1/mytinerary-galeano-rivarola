7d7e70394a9336388e3a166286ccac42aa964728
juanrivarola <jeryflr@gmail.com>
2022-08-31 14:06:55 -0300

    saque datos innecesarios

--- src/components/CardCity.js
+++ src/components/CardCity.js
@@ -12,11 +12,11 @@ function CardCity(props){
             <div className='CardCity-info'>
                 <img src={item.image} className="CardCity-img" />
                 <p className="CardCity-city">{item.name}</p>
-                <p className="CardCity-country">{item.country}</p>
+                {/* <p className="CardCity-country">{item.country}</p>
                 <div className='cardCity-info'>
                     <p className='cardCity-population'>Population: {item.population.toLocaleString("de-DE")}</p>
                     <p className='cardCity-fundation'>Fundation: {item.fundation.toLocaleString("de-DE")}</p>
-                </div>
+                </div> */}
             </div>
         </div>
             </LinkRouter>
