0cf2acfcca173232a43b7d7b690c4fd1be08635c
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-01 15:37:42 -0300

    se borraron comentarios innecesarios

--- src/components/CountryCarousel.js
+++ src/components/CountryCarousel.js
@@ -4,20 +4,7 @@ import Carousel from "./Carousel";
 
 
 function CountryCarousel (){
-//     const itemss =[
-//         {url: "/img/Athens.jpg", country: "Athens"},
-//         {url: "/img/BuenosAires.jpg", country: "Buenos Aires"},
-//         {url: "/img/Havana.jpg", country: "Havana"},
-//         {url: "/img/LasVegas.jpg", country: "Las Vegas"},
-//         {url: "/img/Lima.jpg", country: "Lima"},
-//         {url: "/img/London.jpg", country: "London"},
-//         {url: "/img/LosAngeles.jpg", country: "Los Angeles"},
-//         {url: "/img/Paris.jpg", country: "Paris"},
-//         {url: "/img/RioDeJaneiro.jpg", country: "Rio de Janeiro"},
-//         {url: "/img/Rome.jpg", country: "Rome"},
-//         {url: "/img/Sydney.jpg", country: "Sydney"},
-//         {url: "/img/Tokyo.jpg", country: "Tokyo"},
-// ]
+
 
 const[items, setItems] = useState([])
 useEffect(()=>{
@@ -29,8 +16,6 @@ axios.get('http://localhost:4000/cities/')
         <>
             <h1 className='Carousel-title'>Popular Cities</h1>
             <Carousel data={items} range={4} slides={3} interval={5}/>
-            {/* <Carousel data={items} rangeStart={4} rangeEnd={8}/>
-            <Carousel data={items} rangeStart={8} rangeEnd={12}/>  */}
         </>
     )
 }
