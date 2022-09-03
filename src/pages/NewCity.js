01497aa494ccc0f8fa5c57360a53ee21327f768f
juanrivarola <jeryflr@gmail.com>
2022-09-03 18:52:45 -0300

    new city funcional

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,5 +1,6 @@
 import '../styles/NewCity.css'
 import Input from '../components/Input'
+import axios from 'axios'
 
 export default function NewCity(){
     const items = [
@@ -18,16 +19,24 @@ export default function NewCity(){
         let inputImage= document.getElementById("Image").value
         let inputPopulation= document.getElementById("Population").value
         let inputFundation= document.getElementById("Fundation").value
-        let valor = { City: inputCity , Country : inputCountry, Image: inputImage,Population:inputPopulation, Fundation: inputFundation}
-        let resJson = JSON.stringify(valor)
-        console.log(resJson)
-        fetch('http://localhost:4000/cities/',{
-            method: 'Post',
-            body: resJson
-        }).then(x => console.log(x))
-    }
-    
-    
+        //let valor = { City: inputCity , Country : inputCountry, Image: inputImage,Population:inputPopulation, Fundation: inputFundation}
+        //let resJson = valor
+        //console.log(resJson)
+
+             axios.post(`http://localhost:4000/cities/`,{           
+                  name: inputCity,
+                  country : inputCountry,
+                  image: inputImage,
+                  population: parseInt(inputPopulation),
+                  fundation: parseInt(inputFundation)
+                })
+                .then(function(response){
+                    console.log(response)
+                })
+                .catch(function(error){
+                    console.log(error)
+                })
+                }
      return(
         <div className='new-city-container'>
             <h1 className='nc-title'>New City</h1>
