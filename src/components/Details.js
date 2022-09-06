eeb74a6936f3cf4d436c3de18aa0130476f93f5d
juanrivarola <jeryflr@gmail.com>
2022-09-06 14:44:01 -0300

    se imprimen los itineraries

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,21 +2,28 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import InputDetails from './InputDetails';
+import Itinerary from './Itinerary';
 
 export default function Details() {
   const {id} = useParams();
   const[items, setItems] = useState({})
+  const[itineraries, setItineraries] = useState([])
   useEffect(()=>{
   axios.get(`http://localhost:4000/cities/${id}`)
       .then(response =>setItems(response.data.response))
-    }, [])
+      axios.get(`http://localhost:4000/itineraries/query?city=${id}`)
+      .then(response =>setItineraries(response.data.response))
+    },[])
   
-    
-    // console.log(items);
+
+       axios.get(`http://localhost:4000/itineraries/query?city=${id}`)
+           .then(response =>setItineraries(response.data.response))
+
 
   return (
   <>
     <InputDetails data={items}/>
+    <Itinerary data={itineraries}/>
   </>
   )
 }
