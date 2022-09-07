1d00cfd50bc697bbd31e9832e98243186cb4f535
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-07 15:18:23 -0300

    se renderizo como antes el carousel

--- src/components/CountryCarousel.js
+++ src/components/CountryCarousel.js
@@ -1,28 +1,44 @@
 
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import cityActions from "../features/cityActions";
 import Carousel from "./Carousel";
-
+import { useAllQuery } from "../features/citiesApi";
+import axios from "axios";
 
 function CountryCarousel (){
 
+    // let {
+    //     data:cities,
+    //     error,
+    //     isLoading,
+    //     isSuccess,
+    //     isFailed
+    // } = useAllQuery()
+    // let content
+    // if(isLoading){
+    //     cities = []
+    // }else if(isSuccess){
+    //     cities = cities.response
+    // }else if(isFailed){
+    //     cities = []
+    // }
 
-// const[items, setItems] = useState([])
-// useEffect(()=>{
-// axios.get('http://localhost:4000/cities/')
-//     .then(response => setItems(response.data.response))
-// }, [])
-
-
-const items = useSelector((state)=>
-    state.citiesSlice.cities
-)
-const dispatch = useDispatch()
-//console.log(items)
+const[items, setItems] = useState([])
 useEffect(()=>{
-    dispatch(cityActions.all())
-},[])
+axios.get('http://localhost:4000/cities/')
+    .then(response => setItems(response.data.response))
+}, [])
+
+
+// const items = useSelector((state)=>
+//     state.citiesSlice.cities
+// )
+// const dispatch = useDispatch()
+// //console.log(items)
+// useEffect(()=>{
+//     dispatch(cityActions.all())
+// },[])
 
 
     return (
