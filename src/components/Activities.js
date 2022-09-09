23fe39450bcfef63ed6a0cd775b03352dd3af2b3
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-09 01:03:22 -0300

    se acomodo el orden de los divs

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -3,16 +3,20 @@ function Activity(props){
     const allCard = props.data
 
     const cardActivity = (item) =>(
-        <div className="act-card">
-            <h2 className="act-photo">{item.photo}</h2>
-            <p className="act-name">{item.name}</p>
-        </div>
+                <div className="act-card">
+                    <h2 className="act-photo">{item.photo}</h2>
+                    <p className="act-name">{item.name}</p>
+                </div>
     )
 
     return(
-        <>
-        {allCard.map(cardActivity)}
-    </>
+        <div className='activity'>
+            <h2 className='activity-title'>Activities</h2>
+            <div className='activity-container'>
+                {allCard.map(cardActivity)}
+            </div>
+        </div>
+        
     )
 }
 
