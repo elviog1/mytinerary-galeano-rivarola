d0c5f94d85c0e8e9971ee8701f51f02076c01d9b
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-13 15:32:18 -0300

    modificaciones y renderizados

--- src/components/SocketForm.js
+++ src/components/SocketForm.js
@@ -7,13 +7,17 @@ const socket = io.connect("http://localhost:3000")
 function SocketForm (){
     const [username,setUsername] = useState("")
     const [room,setRoom] = useState("")
+    const [showChat,setShowChat] = useState(false)
 
     const joinRoom = ()=>{
         if(username !== "" && room !== ""){
             socket.emit("join_room", room)
+            setShowChat(true)
         }
     }
     return (
+        <div>
+            {!showChat ? (
         <div className="socketForm-window">
             <h3 className="socketForm-title">Join a chat</h3>
             <div className='socketForm-inputs'>
@@ -21,9 +25,11 @@ function SocketForm (){
                 <input placeholder="Room ID..."  type="text" onChange={e =>{setRoom(e.target.value)}}/>
             </div>
             <button className="socketForm-button" onClick={joinRoom}>Join a Room</button>
-            <Chat socket={socket} username={username} room={room} />
         </div>
-
+            ):(
+            <Chat socket={socket} username={username} room={room} />
+            )}
+        </div>
     )
 }
 
