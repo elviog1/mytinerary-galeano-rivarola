0fe729ee1d3c4a868e81be3d53fc63b57017df5d
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-20 01:29:46 -0300

    se puede publicar comentarios

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,22 +1,25 @@
 import axios from "axios"
 import { useRef } from "react"
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 
-
-function NewComment(item){
+function NewComment(props){
+    const itineraryId = props.itinerary
     const divText = useRef(null)
-
     const sendComment = ()=>{
-        // axios.post(`http://localhost:4000/comments`,{
-        // user: JSON.parse(localStorage.getItem('user')).id,
-        // itinerary: " " ,
-        // comment: divText.current.value
-
-        // })
+        axios.post(`http://localhost:4000/comments`,{
+        user: JSON.parse(localStorage.getItem('user')).id,
+        itinerary: itineraryId ,
+        comment: divText.current.value
+        })
+        Toastify({
+            text:"Thanks for your comment!"
+        }).showToast()
     }
 
     return (
         <div className="comment-info">
-            <textarea ref={divText} className="comment-content"></textarea>
+            <textarea ref={divText} style={{resize:"none"}} className="comment-content"></textarea>
             <div className='comment-close' onClick={()=>sendComment()} >Create</div>
         </div>
     )
