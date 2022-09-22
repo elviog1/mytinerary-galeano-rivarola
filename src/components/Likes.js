c4b32135f2665221b7f1e7c4627d07e6fb5fc6d6
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-22 02:38:51 -0300

    se comento todo

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -3,36 +3,36 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 function Likes (props){
-    let userId = JSON.parse(localStorage.getItem('user')).id 
-    const [likes, setLikes]= useState()
-    let itinerary = props.itinerary
-    let token = JSON.parse(localStorage.getItem('token'))
-    //console.log(token)
-    const [like,setLike] = useState(false)
-    let header={headers: { 'Authorization': `Bearer ${token}`}}
-    let urlR= `http://localhost:4000/itineraries/like/`+itinerary
-    useEffect(()=>{
-        axios.get(`http://localhost:4000/itineraries/${itinerary}`)
-        .then(response => setLikes(response.data.response.likes.length))
-    },[])
+    // let userId = JSON.parse(localStorage.getItem('user')).id 
+    // const [likes, setLikes]= useState()
+    // let itinerary = props.itinerary
+    // let token = JSON.parse(localStorage.getItem('token'))
+    // //console.log(token)
+    // const [like,setLike] = useState(false)
+    // let header={headers: { 'Authorization': `Bearer ${token}`}}
+    // let urlR= `http://localhost:4000/itineraries/like/`+itinerary
+    // useEffect(()=>{
+    //     axios.get(`http://localhost:4000/itineraries/${itinerary}`)
+    //     .then(response => setLikes(response.data.response.likes.length))
+    // },[])
 
-    console.log(likes)
-    const click=async ()=>{
-        setLike(!like)
-        console.log(like)
+    // console.log(likes)
+    // const click=async ()=>{
+    //     setLike(!like)
+    //     console.log(like)
 
-        axios.patch(urlR, header
-        ).then((res) => {
-            console.log(res.data)
-          })
-          .catch((error) => {
-            console.error(error)
-          })
-    }
+    //     axios.patch(urlR, header
+    //     ).then((res) => {
+    //         console.log(res.data)
+    //       })
+    //       .catch((error) => {
+    //         console.error(error)
+    //       })
+    // }
 
     return (
         <div>
-            <button onClick={click}>♡{likes}</button>
+            {/* <button onClick={click}>♡{likes}</button> */}
         </div>
     )
 }
