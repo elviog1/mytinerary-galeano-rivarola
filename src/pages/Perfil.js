96e2c66de533bd61dcc8623e7da334a17868c4dd
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-21 23:09:01 -0300

    se creo el componente del perfil del usuario

---
+++ src/pages/Perfil.js
@@ -0,0 +1,22 @@
+import '../styles/Perfil.css'
+function Perfil (){
+
+    let info = JSON.parse(localStorage.getItem("user"))
+
+    return(
+        <div className='new-city-container'>
+            <h1 className='nc-title'>Perfil</h1>
+            <div className='nc-form'>
+                <img src={info.photo} className="perfi-img"/>
+                <div className="perfil-description">
+                    <p className='perfil-info'>Name: <span className='perfil-span'>{info.name}</span></p>
+                    <p className='perfil-info'>Country: <span className='perfil-span'>{info.country}</span></p>
+                    <p className='perfil-info'>Email: <span className='perfil-span'>{info.mail}</span></p>
+                    <p className='perfil-info'>Role: <span className='perfil-span'>{info.role}</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Perfil
